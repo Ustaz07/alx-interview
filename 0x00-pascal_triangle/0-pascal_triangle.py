0759552ca068a2618f4c6198ce9cb6878c8cc508
Ustaz07 <shamsutukor@gmail.com>
2024-08-11 07:13:04 -0700

    0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,14 +4,16 @@
 """
 
 def pascal_triangle(n):
-    """Returns a list of lists of integers representing Pascal’s triangle of n"""
+    """
+    Returns a list of lists of integers representing Pascal’s triangle of n.
+    """
     res = []
     if n > 0:
         for i in range(1, n + 1):
             level = []
-            C = 1
+            c = 1  # Changed variable name to lowercase to follow PEP 8
             for j in range(1, i + 1):
-                level.append(C)
-                C = C * (i - j) // j
+                level.append(c)
+                c = c * (i - j) // j
             res.append(level)
     return res
