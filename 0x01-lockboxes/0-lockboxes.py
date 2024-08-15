554f7b954db129542eb94210f10f76e9aefbcdce
Ustaz07 <shamsutukor@gmail.com>
2024-08-15 14:23:48 -0700

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,43 +1,20 @@
 #!/usr/bin/python3
-"""Solves the lock boxes puzzle"""
+"""
+Method to determine if all boxes can be opened
+Using prototype: def canUnlockAll(boxes)
+"""
 
-from collections import deque
 
 def canUnlockAll(boxes):
-    """Check if all boxes can be opened
-    Args:
-        boxes (list): List where each element is a list of keys for that box.
-    Returns:
-        bool: True if all boxes can be opened, otherwise False.
     """
-    if not boxes:
-        return False
-
-    n = len(boxes)
-    opened = [False] * n  # Track which boxes have been opened
-    queue = deque([0])    # Start with the first box (index 0)
-
-    while queue:
-        current_box = queue.popleft()
-        if not opened[current_box]:
-            opened[current_box] = True
-            for key in boxes[current_box]:
-                if key < n and not opened[key]:
-                    queue.append(key)
-
-    return all(opened)
-
-def main():
-    """Entry point"""
-    boxes = [[1], [2], [3], [4], []]
-    print(canUnlockAll(boxes))  # Expected output: True
-
-    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(canUnlockAll(boxes))  # Expected output: True
-
-    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes))  # Expected output: False
-
-if __name__ == '__main__':
-    main()
-
+    Check if boxes can be unlocked
+    """
+    for key in range(1, len(boxes) - 1):
+        ctr = False
+        for idx in range(len(boxes)):
+            ctr = (key in boxes[idx] and key != idx)
+            if ctr:
+                break
+        if ctr is False:
+            return ctr
+    return True
